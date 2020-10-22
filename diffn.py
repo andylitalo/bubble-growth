ad13fc13e776b236eb2dbe1cfe75c636d567d5aa
Andy Ylitalo <aylitalo@caltech.edu>
2020-10-21 21:27:26 -0700

    fixed bugs in diffn.py, polyco2.py
    polyco2.calc_D_of_c now returns a float instead of an array

--- diffn.py
+++ diffn.py
@@ -56,7 +56,7 @@ def dirichlet(c, i, val):
     c[i] = val
 
 
-def calc_dcdt_cyl(r_arr, c_arr, dr, dc, fixed_params):
+def calc_dcdt_cyl(r_arr, c_arr, fixed_params):
     """
     Computes the time derivative of concentration dc/dt in cylindrical
     coordinates assuming concentration dependent diffusivity constant.
@@ -74,13 +74,11 @@ def calc_dcdt_cyl(r_arr, c_arr, dr, dc, fixed_params):
     c_arr : numpy array of N+1 floats
         concentrations of CO2 at each of the points in r_arr
         [kg CO2 / m^3 polyol-co2]
-    dr : float
-        mesh spacing [m]
-    dc : float
-        step size of concentration used to estimate dD/dc by forward difference
-        formula [kg CO2 / m^3 polyol-CO2]
+
     fixed_params: list
-        only contains:
+        dc : float
+            step size of concentration used to estimate dD/dc by forward
+            difference formula [kg CO2 / m^3 polyol-CO2]
         polyol_data_file : string
             name of file containing polyol data [.csv]
 
@@ -92,30 +90,32 @@ def calc_dcdt_cyl(r_arr, c_arr, dr, dc, fixed_params):
         the boundary conditions)
     """
     # extracts fixed parameters
-    polyol_data_file = fixed_params[0]
+    dc, polyol_data_file = fixed_params
     # extracts just the concentrations that will change (i = 1...N-1)
     c_arr_c = c_arr[1:-1]
     # and their corresponding radii
     r_arr_c = r_arr[1:-1]
+    # and their corresponding grid spacings
+    dr_arr = r_arr[2:] - r_arr[1:-1]
 
     # FIRST TERM: 1/r * D(c) * dc/dr
     # computes diffusivity at each point
-    D_arr = np.asarray([polyco2.calc_D(c, polyol_data_file) \
-                                                    for c in list(c_arr_c)])
+    D_arr = np.asarray([polyco2.calc_D_of_c(c, polyol_data_file) \
+                                                    for c in c_arr_c])
     # computes spatial derivative of concentration dc/dr with leapfrog method
-    dcdr_arr = (c_arr[2:] - c_arr[:-2]) / (2*dr)
+    dcdr_arr = (c_arr[2:] - c_arr[:-2]) / (2*dr_arr)
     term1 = 1/r_arr_c * D_arr * dcdr_arr
 
     # SECOND TERM: dD/dc * (dc/dr)^2
     # computes dD/dc [m^2/s / kg/m^3]
     dDdc_arr = np.asarray([polyco2.calc_dDdc(c, dc, polyol_data_file) \
-                                                    for c in list(c_arr_c)])
+                                                    for c in c_arr_c])
     term2 = dDdc_arr * (dcdr_arr)**2
 
     # THIRD TERM: D(c) * d2c/dr2
     # computes second spatial derivative of concentration with central
     # difference formula
-    d2cdr2_arr = (c_arr[2:] - 2*c_arr[1:-1] + c_arr[:-2]) / (dc**2)
+    d2cdr2_arr = (c_arr[2:] - 2*c_arr[1:-1] + c_arr[:-2]) / (dr_arr**2)
     term3 = D_arr * d2cdr2_arr
 
     dcdt = term1 + term2 + term3
@@ -123,7 +123,7 @@ def calc_dcdt_cyl(r_arr, c_arr, dr, dc, fixed_params):
     return dcdt
 
 
-def go(dt, t_f, r_min, r_max, N, c_0, dcdt_fn, bc_specs_list, polyol_data_file):
+def go(dt, t_f, r_arr, c_0, dcdt_fn, bc_specs_list, dc, polyol_data_file):
     """
     Diffuses CO2 throughout system.
 
@@ -134,12 +134,8 @@ def go(dt, t_f, r_min, r_max, N, c_0, dcdt_fn, bc_specs_list, polyol_data_file):
         time step (might be adjusted if adaptive time-stepping) [s]
     t_f : float
         final time [s]
-    r_min : float
-        minimum radius of domain [m]
-    r_max : float
-        maximum radius of domain [m]
-    N : int
-        number of steps to make grid (N+1 grid points)
+    r_arr : numpy array of N+1 floats
+        mesh points [m]
     c_0 : list of N+1 floats
         initial concentration profile of CO2 at grid points
     dcdt_fn : function
@@ -147,6 +143,9 @@ def go(dt, t_f, r_min, r_max, N, c_0, dcdt_fn, bc_specs_list, polyol_data_file):
     bc_specs_list : list of tuples
         List of tuples of specs for boundary conditions (first element of each
         tuple is the function for applying the boundary condition)
+    dc : float
+        Step size in concentration for estimating dD/dc with forward difference
+        formula [kg CO2 / m^3 polyol-CO2]
     polyol_data_file : string
         name of file containing polyol-CO2 data [.csv]
 
@@ -154,8 +153,6 @@ def go(dt, t_f, r_min, r_max, N, c_0, dcdt_fn, bc_specs_list, polyol_data_file):
     -------
     t : list of M floats
         times at which each of the M concentration profiles are evaluated
-    r_arr : numpy array of N+1 floats
-        grid points [m]
     c : list of M lists of N+1 floats
         concentration profiles over time [kg CO2 / m^3 polyol-CO2], M time
         steps and N points in space
@@ -163,10 +160,9 @@ def go(dt, t_f, r_min, r_max, N, c_0, dcdt_fn, bc_specs_list, polyol_data_file):
     # initializes system
     t = [0]
     c = [c_0]
-    r_arr = np.arange(0, N+1, 1)*dr
 
     # stores fixed parameters
-    fixed_params = (polyol_data_file)
+    fixed_params = (dc, polyol_data_file)
     # applies Euler's method to estimate bubble growth over time
     # the second condition provides cutoff for shrinking the bubble
     while t[-1] <= t_f:
@@ -176,20 +172,20 @@ def go(dt, t_f, r_min, r_max, N, c_0, dcdt_fn, bc_specs_list, polyol_data_file):
         # stores properties at new time step in lists
         update_props(props, t, c)
 
-    return t, r_arr, c
+    return t, c
 
 
-def neumann(c, i, j, dcdr, dr):
+def neumann(c, i, j, dcdr, r_arr):
     """
     Applies Neumann boundary condition at desired index.
 
     The condition is
 
-                    dc/dr_i = (c_i - c_j) / dr
+                    dc/dr_i = (c_i - c_j) / (r_i - r_j)
 
     so we can solve for the concentration at c[i] (the boundary) as
 
-                    c[i] = dc/dr_i * dr + c_j
+                    c[i] = dc/dr_i * (r_i - r_j) + c_j
 
     Parameters
     ----------
@@ -202,11 +198,11 @@ def neumann(c, i, j, dcdr, dr):
     dcdr : float
         value of dc/dr spatial derivative of the concentration at the boundary
         [kg CO2 / m^3 polyol-CO2]
-    dr : float
-        grid spacing (used to compute derivative with difference formula) [m]
+    r_arr : numpy array of N+1 floats
+        mesh points [m]
     """
     # solves boundary condition for the value at the boundary
-    c[i] = dcdr * dr + c[j]
+    c[i] = dcdr * (r_arr[i] - r_arr[j]) + c[j]
 
 
 def time_step(dt, t_prev, r_arr, c_prev, dcdt_fn, bc_specs_list, fixed_params):
@@ -239,25 +235,31 @@ def time_step(dt, t_prev, r_arr, c_prev, dcdt_fn, bc_specs_list, fixed_params):
 
     Returns
     -------
-    dt : float
+    dt_curr : float
         time step (might be different than input if adaptive time-stepping used)
         [s]
-    t : float
+    t_curr : float
         current time [s] (t_prev + dt)
-    c : list of N+1 floats
+    c_curr : list of N+1 floats
         current concentration profile at grid points [kg CO2 / m^3 polyol-CO2]
     """
     # increments time forward [s]
-    t = t_prev + dt
+    t_curr = t_prev + dt
+    # no adaptive time-stepping
+    dt_curr = dt
+    # extracts array of relevant concentrations
+    c_prev_arr = np.array(c_prev[1:-1])
 
     # updates concentration with explicit Euler method
-    c = c_prev + dt*dcdt_fn(r_arr, np.array(c_prev), dr, dc, fixed_params)
+    c_curr_arr = c_prev_arr + dt*dcdt_fn(r_arr, np.array(c_prev), fixed_params)
+    # adds end points for the application of the boundary conditions
+    c_curr = [0] + list(c_curr_arr) + [0]
 
     # applies boundary conditions
     for bc_specs in bc_specs_list:
-        apply_bc(c, bc_specs)
+        apply_bc(c_curr, bc_specs)
 
-    return dt, t, c
+    return dt_curr, t_curr, c_curr
 
 
 def update_props(props, t, c):
